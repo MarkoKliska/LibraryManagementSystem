7ff76591aece516dc92afdeeefc5b3d657709869
MarkoKliska <021markokliska@gmail.com>
2026-07-07 18:39:05 +0200

    Fixed security issues and prepared for building a mail searvice

---
+++ backend/LibraryManagementSystem/src/Mail.Service/LibraryManagementSystem.Contracts/IntegrationEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementSystem.Contracts;
+
+public abstract record IntegrationEvent
+{
+    public Guid EventId { get; set; } = Guid.NewGuid();
+    public DateTime OccuredAtUtc { get; set; } = DateTime.UtcNow;
+}
---
+++ backend/LibraryManagementSystem/src/Mail.Service/LibraryManagementSystem.Contracts/User/UserRegisteredIntegrationEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace LibraryManagementSystem.Contracts.User;
+
+public record UserRegisteredIntegrationEvent(
+    Guid UserId,
+    string Email, 
+    string FirstName) : IntegrationEvent;
